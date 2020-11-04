38a8b1e5cfda6c16140cbe305ae9264d8c035d63
tomasdetloging <tomasdetloging@gmail.com>
2020-11-04 11:00:11 -0400

    add api link for development

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-export const serverUrl = "https://openacademy-api.herokuapp.com";
+export const serverUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "https://openacademy-api.herokuapp.com";
 
 export const apiUrl = serverUrl + "";
 export const apiLoginUrl = serverUrl + "/login";
